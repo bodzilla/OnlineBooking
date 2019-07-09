77fe7e8b9f052de37956cf38d34ee5dfd5845512
Bodrul <bodrul.ic@msn.com>
2019-07-09 19:30:18 +0100

    Make collection protected instead of private.

--- OnlineBooking.Web/Repositories/GenericRepository.cs
+++ OnlineBooking.Web/Repositories/GenericRepository.cs
@@ -10,33 +10,33 @@ namespace OnlineBooking.Web.Repositories
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
-        private readonly IMongoCollection<T> _collection;
+        protected readonly IMongoCollection<T> Collection;
 
-        public GenericRepository(DbContext context) => _collection = context.Database.GetCollection<T>(typeof(T).Name);
+        public GenericRepository(DbContext context) => Collection = context.Database.GetCollection<T>(typeof(T).Name);
 
-        public async Task<IEnumerable<T>> GetAllAsync() => await _collection.Find(x => true).ToListAsync();
+        public async Task<IEnumerable<T>> GetAllAsync() => await Collection.Find(x => true).ToListAsync();
 
-        public async Task<IEnumerable<T>> GetListAsync(Expression<Func<T, bool>> predicate) => await _collection.Find(predicate).ToListAsync();
+        public async Task<IEnumerable<T>> GetListAsync(Expression<Func<T, bool>> predicate) => await Collection.Find(predicate).ToListAsync();
 
-        public async Task<T> GetAsync(Expression<Func<T, bool>> predicate) => await _collection.Find(predicate).FirstOrDefaultAsync();
+        public async Task<T> GetAsync(Expression<Func<T, bool>> predicate) => await Collection.Find(predicate).FirstOrDefaultAsync();
 
         public async Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate) => await GetAsync(predicate) != null;
 
         public async Task<T> CreateAsync(T entity)
         {
-            await _collection.InsertOneAsync(entity);
+            await Collection.InsertOneAsync(entity);
             return entity;
         }
 
         public async Task<bool> UpdateAsync(string id, T entity)
         {
-            var result = await _collection.ReplaceOneAsync(x => x.Id.Equals(id), entity);
+            var result = await Collection.ReplaceOneAsync(x => x.Id.Equals(id), entity);
             return result.IsAcknowledged;
         }
 
         public async Task<bool> DeleteAsync(string id)
         {
-            var result = await _collection.DeleteOneAsync(x => x.Id.Equals(id));
+            var result = await Collection.DeleteOneAsync(x => x.Id.Equals(id));
             return result.IsAcknowledged;
         }
     }
